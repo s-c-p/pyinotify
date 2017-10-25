877e3ed71e2af6a4e18a29527963e01d4dbc1db5
s-c-p <prasadsatish45@binkmail.com>
2017-10-25 20:11:49 +0530

    yo

--- not_quiet.py
+++ not_quiet.py
@@ -7,30 +7,29 @@ import pyinotify
 
 wm = pyinotify.WatchManager()
 
-
 # default behavior, don't complain but keep trace of error in log and result
 r = wm.add_watch(['/tmp', '/tmp-do-not-exist'], pyinotify.ALL_EVENTS)
-print r
+print(r)
 
 
 # quiet=False raise exception
 try:
     wm.add_watch(['/tmp', '/tmp-do-not-exist'],
                      pyinotify.ALL_EVENTS, quiet=False)
-except pyinotify.WatchManagerError, err:
-    print err, err.wmd
+except pyinotify.WatchManagerError as err:
+    print(err)
 
-
+"""
 # quiet=False raise exception
 try:
     wm.update_watch(42, mask=0x42, quiet=False)
-except pyinotify.WatchManagerError, err:
-    print err, err.wmd
+except pyinotify.WatchManagerError as err:
+    print(err, err.wmd)
 
 
 # quiet=False raise exception
 try:
     wm.rm_watch(42, quiet=False)
-except pyinotify.WatchManagerError, err:
-    print err, err.wmd
-
+except pyinotify.WatchManagerError as err:
+    print(err)
+"""
