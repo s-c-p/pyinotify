158eeea61968961bc4211429f9556c786ec8f602
s-c-p <prasadsatish45@binkmail.com>
2017-10-26 20:24:48 +0530

    yo

--- stats.py
+++ stats.py
@@ -3,6 +3,11 @@
 import pyinotify
 
 class Identity(pyinotify.ProcessEvent):
+    """ think of this as a collection of suite-of-functions that are executed
+    whenever a FS-event occours. If it needs anything from outside at the time
+    of instanciation, define it in my_init IFF that thing is defined under the
+    ProcessEvent class (e.g. pyinotofy.Stats()) you can skip it directly
+    """
     def process_default(self, event):
         # Does nothing, just to demonstrate how stuffs could trivially
         # be accomplished after having processed statistics.
@@ -22,7 +27,7 @@ wm = pyinotify.WatchManager()
 notifier = pyinotify.Notifier(
     wm,
     read_freq=1,
-    default_proc_fun=proc_function
+    default_proc_fun=proc_function # calls the suite of functions defined in Identity
 )
 wm.add_watch(
     '/var/log',
